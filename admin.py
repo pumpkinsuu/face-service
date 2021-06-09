317fc7eeac1722962af49e32af2173a569a610e6
pumpkinsuu <pumpkinsuu@gmail.com>
2021-06-09 16:48:32 +0700

    update

--- admin.py
+++ admin.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_cors import CORS
-import os
 
 from config.admin import *
 
@@ -8,9 +7,7 @@ from routes.admin import create_admin_bp
 from utilities import ErrorAPI, logger, response
 log = logger('face')
 
-templates = os.path.abspath('templates')
-print(templates)
-app = Flask(__name__, static_folder='static', template_folder=templates)
+app = Flask(__name__, static_folder='static')
 CORS(app)
 app.secret_key = SECRET
 
