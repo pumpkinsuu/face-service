3330304732478dc6dfc3e63fe48948da483bba01
pumpkinsuu <pumpkinsuu@gmail.com>
2021-06-09 17:13:06 +0700

    update

--- routes/admin.py
+++ routes/admin.py
@@ -7,8 +7,8 @@ from config.admin import *
 
 def create_admin_bp(app):
     admin_bp = Blueprint('admin_bp', __name__,
-                         template_folder='../templates',
-                         static_folder='../static')
+                         template_folder='templates',
+                         static_folder='static')
 
     db = AdminData(app)
     login_manager = LoginManager()
